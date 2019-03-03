5912870dd133f0e139818ae817d30b2047305d8d
Roland Becker <roland.becker@univ-pau.fr>
2019-03-03 09:03:50 +0100

    rajout

--- leslie.py
+++ leslie.py
@@ -2,19 +2,30 @@ from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
 
+def exemple1():
+    ngroupes=3
+    u = np.zeros(ngroupes)
+    u[0]=1
+    A = np.empty(shape=(ngroupes,ngroupes))
+    A[0] = [0, 1, 5]
+    A[1] = [.3, 0, 0]
+    A[2] = [0, .5, 0]
+    print("A",A)
+    return A, u
 
-niter=20
-ngroupes=3
+def exemple2():
+    ngroupes=2
+    u = np.zeros(ngroupes)
+    u[0]=1
+    A = np.empty(shape=(ngroupes,ngroupes))
+    A[0] = [0, 2]
+    A[1] = [.75, 0]
+    print("A",A)
+    return A, u
 
-u = np.zeros(ngroupes)
-u[1]=1
-A = np.empty(shape=(ngroupes,ngroupes))
-A[0] = [0,1,5]
-A[1] = [.3, 0, 0]
-A[2] = [0, .5, 0]
-print("A",A)
-
+A, u = exemple2()
 uall = []
+niter = 40
 for i in range(niter):
     uall.append(u)
     u = A.dot(u)
